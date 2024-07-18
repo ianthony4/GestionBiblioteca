2df889636bffd10c27c7572fa3d54fa013505ccc
ianthony4 <achaisa@unsa.edu.pe>
2024-07-18 16:41:18 -0500

    Creando la estructura LIBRO

---
+++ libro.h
@@ -0,0 +1,14 @@
+#ifndef LIBRO_H
+#define LIBRO_H
+
+#include <string>
+
+struct Libro {
+    std::string codigo;
+    std::string nombre;
+    std::string autor;
+    int anioPublicacion
+    double precio;
+};
+
+#endif 
