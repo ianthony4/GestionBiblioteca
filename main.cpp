5bf034f8a412791cd2c00b210604cbc51b2689a5
achaisa <achaisa@unsa.edu.pe>
2024-07-18 21:57:20 -0500

    creando metodo para listar libros

--- main.cpp
+++ main.cpp
@@ -15,8 +15,22 @@ void agregarLibro(){
     std::getline(std::cin, libro.autor);
     std::cout << "Ingrese el año de publicacion: ";
     std::cin >> libro.anioPublicacion;
-    std::cout "Ingrese el precio del libro: ";
+    std::cout << "Ingrese el precio del libro: ";
     std::cin >> libro.precio;
     
     libros.push_back(libro);
+}
+
+//Aqui se listara los libros
+void listarLibros(){
+    //for each para recorrer los libros
+    for(const auto& libro : libros){
+        std::cout << "Codigo: " << libro.codigo << std::endl;
+        std::cout << "Nombre: " << libro.nombre << std::endl;
+        std::cout << "Autor: " << libro.autor << std::endl;
+        std::cout << "Año de publicacion: " << libro.anioPublicacion << std::endl;
+        std::cout << "Precio: " << libro.precio << std::endl;
+        std::cout << "--------------------------------" << std::endl;  
+    }
+    
 }
